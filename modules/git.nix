c15a92ee0bf481b9b265bee23eba54c89ffe80c0
Jens Sigvardsson <19902144+jensjensjens@users.noreply.github.com>
2023-11-24 20:17:03 +0100

    Cleaned up git config

--- modules/git.nix
+++ modules/git.nix
@@ -10,20 +10,16 @@
       commit-undo = "reset --soft HEAD^";
       log-tag = "log --oneline --decorate --tags --no-walk";
       log-tree-all = "log --oneline --decorate --graph --all";
-      log-tree =
-        "log --all --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+      log-tree = "log --all --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
       pfetch = "fetch --prune";
-      violence =
-        "git add . && git commit --amend --no-edit && git push --force-with-lease";
+      violence = "git add . && git commit --amend --no-edit && git push --force-with-lease";
       log-new = "log main..HEAD";
       log-old = "log HEAD..main";
     };
     includes = [{ path = "~/.config/git/config-work"; }];
     extraConfig = {
-      user = { signingkey = "~/.ssh/github-personal.pub"; };
       core = {
         editor = "nvim";
-        sshCommand = "ssh -i ~/.ssh/github-personal";
       };
       push = { default = "current"; };
       commit = { gpgsign = true; };
